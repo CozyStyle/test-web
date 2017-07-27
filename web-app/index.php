9dc1386a96e331bff440f4af355be30a4e9c3814
CozyStyle <k.ishi@nekonet.co.jp>
2017-07-27 16:03:10 +0900

    構文エラーにしてみた
    んですけど？

--- web-app/index.php
+++ web-app/index.php
@@ -9,6 +9,7 @@
   $FG   = array($MM - $BG[0], $MM - $BG[1], $MM - $BG[2]);
   $BGS  = vsprintf('#%02x%02x%02x', $BG);
   $FGS  = vsprintf('#%02x%02x%02x', $FG);
+  $ERR  = "E" . "R" . "R" . ...
 ?>
 <head>
   <meta http-equiv="refresh" content="5;URL=./">
